aa8c0c63c1a10aadb48f1b1a7efa76536f644f2a
b177y <34008579+b177y@users.noreply.github.com>
2021-12-15 20:34:09 +0000

    Start outline of mock driver

---
+++ driver/mock/machine.go
@@ -0,0 +1,59 @@
+package uml
+
+import (
+	"github.com/b177y/netkit/driver"
+)
+
+type MockDriver struct {
+	Name         string
+	DefaultImage string
+}
+
+func (md *MockDriver) SetupDriver(conf map[string]interface{}) (err error) {
+	md.Name = "Mock Driver"
+	md.DefaultImage = "MockImage"
+	return nil
+}
+
+func (md *MockDriver) MachineExists(m driver.Machine) (exists bool,
+	err error) {
+	return true, nil
+}
+
+func (md *MockDriver) StartMachine(m driver.Machine) (err error) {
+	return nil
+}
+
+func (md *MockDriver) HaltMachine(m driver.Machine, force bool) error {
+	return nil
+}
+
+func (md *MockDriver) RemoveMachine(m driver.Machine) error {
+	return nil
+}
+
+func (md *MockDriver) GetMachineState(m driver.Machine) (state driver.MachineState, err error) {
+	return state, nil
+}
+
+func (md *MockDriver) AttachToMachine(m driver.Machine) (err error) {
+	return nil
+}
+
+func (md *MockDriver) MachineExecShell(m driver.Machine, command,
+	user string, detach bool, workdir string) (err error) {
+	return driver.ErrNotImplemented
+}
+
+func (md *MockDriver) GetMachineLogs(m driver.Machine,
+	follow bool, tail int) (err error) {
+	return nil
+}
+
+func (md *MockDriver) ListMachines(namespace string, all bool) ([]driver.MachineInfo, error) {
+	return []driver.MachineInfo{}, nil
+}
+
+func (md *MockDriver) MachineInfo(m driver.Machine) (info driver.MachineInfo, err error) {
+	return info, nil
+}
---
+++ driver/mock/network.go
@@ -0,0 +1,38 @@
+package uml
+
+import (
+	"github.com/b177y/netkit/driver"
+)
+
+func (md *MockDriver) CreateNetwork(n driver.Network) (err error) {
+	return err
+}
+
+func (md *MockDriver) StartNetwork(net driver.Network) (err error) {
+	return nil
+}
+
+func (md *MockDriver) RemoveNetwork(net driver.Network) (err error) {
+	return err
+}
+
+func (md *MockDriver) StopNetwork(net driver.Network) (err error) {
+	return nil
+}
+
+func (md *MockDriver) GetNetworkState(net driver.Network) (state driver.NetworkState,
+	err error) {
+	return state, err
+}
+
+func (md *MockDriver) ListNetworks(lab string, all bool) (networks []driver.NetInfo, err error) {
+	return networks, nil
+}
+
+func (md *MockDriver) NetworkExists(net driver.Network) (bool, error) {
+	return false, nil
+}
+
+func (md *MockDriver) NetInfo(net driver.Network) (nInfo driver.NetInfo, err error) {
+	return nInfo, err
+}
