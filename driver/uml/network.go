e880f2185ba5b7bad4eddc0e9a70358a562591f5
b177y <34008579+b177y@users.noreply.github.com>
2021-12-15 18:10:38 +0000

    Start working on UML network driver

--- driver/uml/network.go
+++ driver/uml/network.go
@@ -1,6 +1,12 @@
 package uml
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	"github.com/b177y/netkit/driver"
 )
 
@@ -12,21 +18,26 @@ func (ud *UMLDriver) CreateNetwork(n driver.Network) (err error) {
 	if exists {
 		return driver.ErrExists
 	}
-	return err
+	nHash := fmt.Sprintf("%x",
+		sha256.Sum256([]byte(n.Name+"-"+n.Namespace)))
+	netPath := filepath.Join(ud.RunDir, "network", nHash)
+	err = os.MkdirAll(netPath, 0744)
+	if err != nil && err != os.ErrExist {
+		return err
+	}
+	cmd := exec.Command("uml_switch", "-hub", "-unix", filepath.Join(netPath, "hub.cnct"))
+	return cmd.Start()
 }
 
 func (ud *UMLDriver) StartNetwork(net driver.Network) (err error) {
-	// podman network doesn't need manual starting
 	return nil
 }
 
 func (ud *UMLDriver) RemoveNetwork(net driver.Network) (err error) {
-	// _, err = network.Remove(ud.conn, net.Fullname(), nil)
 	return err
 }
 
 func (ud *UMLDriver) StopNetwork(net driver.Network) (err error) {
-	// podman network doesn't need manual stopping
 	return nil
 }
 
@@ -40,9 +51,18 @@ func (ud *UMLDriver) ListNetworks(lab string, all bool) (networks []driver.NetIn
 	return networks, nil
 }
 
-func (ud *UMLDriver) NetworkExists(net driver.Network) (bool, error) {
-	// return network.Exists(ud.conn, net.Fullname(), nil)
-	return false, nil
+func (ud *UMLDriver) NetworkExists(n driver.Network) (bool, error) {
+	nHash := fmt.Sprintf("%x",
+		sha256.Sum256([]byte(n.Name+"-"+n.Namespace)))
+	hubPath := filepath.Join(ud.RunDir, "network", nHash, "hub.cnct")
+	_, err := os.Stat(hubPath)
+	if err != nil && err != os.ErrNotExist {
+		return false, err
+	} else if err == os.ErrNotExist {
+		return false, nil
+	} else {
+		return true, nil
+	}
 }
 
 func (ud *UMLDriver) NetInfo(net driver.Network) (nInfo driver.NetInfo, err error) {
