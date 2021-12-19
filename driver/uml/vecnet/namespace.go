2e972798c1e8b3accd2a06911ae9b5741bf46c32
b177y <34008579+b177y@users.noreply.github.com>
2021-12-19 12:21:21 +0000

    Support creating named net NSes

--- driver/uml/vecnet/namespace.go
+++ driver/uml/vecnet/namespace.go
@@ -3,21 +3,25 @@ package vecnet
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
+	"sync"
 	"syscall"
 
 	"github.com/containernetworking/plugins/pkg/ns"
+	"golang.org/x/sys/unix"
 )
 
 var NSPID = "/run/user/1000/uml/ns-pid"
 
-// Creates and persists a new user and netnamespace by running a 'pause' process
+// Creates and persists a new user and mount namespace by running a 'pause' process
 // Runs bash with infinite sleep to keep new ns open, as namespaces cannot be bound without root privs
-// The pid is then saved to NSPID which can then be used with /proc/PID/ns/user and /proc/PID/ns/net to enter these namespaces
-// For debugging: `nsenter --preserve-credentials -t PID --user --net`
-func NewNS() error {
+// The pid is then saved to NSPID which can then be used with /proc/PID/ns/user and /proc/PID/ns/mount to enter these namespaces
+// For debugging: `nsenter --preserve-credentials -t PID --user --mount`
+func NewUserNS() error {
 	dir, _ := filepath.Split(NSPID)
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err = os.MkdirAll(dir, 0755)
@@ -31,7 +35,7 @@ func NewNS() error {
 		Path: "/bin/bash",
 		Args: []string{"-i", "-c", "sleep infinity"},
 		SysProcAttr: &syscall.SysProcAttr{
-			Cloneflags: syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+			Cloneflags: syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER, // new mount and user ns
 			UidMappings: []syscall.SysProcIDMap{{
 				ContainerID: 0,
 				HostID:      1000, // TODO use UID
@@ -50,7 +54,7 @@ func NewNS() error {
 // Reexec of current process with nsenter as intermediate
 // before child process runs.
 // Child process will be almost identical but will be running
-// within the UML user namespace opened by NewNS()
+// within the UML user + mount namespace opened by NewNS()
 // Problems with golang's runtime mean that entering a user ns cannot be done
 // natively with syscalls
 // https://github.com/kata-containers/runtime/issues/148
@@ -66,7 +70,7 @@ func EnterUserNS() error {
 		return err
 	}
 	fmt.Println("I was called with", string(cmdline))
-	nsArgs := fmt.Sprintf("/usr/bin/nsenter --preserve-credentials -t %s --user %s", string(pid), "./nettest")
+	nsArgs := fmt.Sprintf("/usr/bin/nsenter --preserve-credentials -t %s --user --mount %s", string(pid), "/home/billy/repos/github.com/b177y/netkit/nettest")
 	bashArgs := []string{"-i", "-c"}
 	// nsArgs = append(nsArgs, fmt.Sprintf("/proc/%d/exe", os.Getpid()))
 	bashArgs = append(bashArgs, nsArgs)
@@ -80,15 +84,67 @@ func EnterUserNS() error {
 	return cmd.Run()
 }
 
+func NewNetNS(name string) error {
+	// setup 'rundir'
+	nsDir := "/run/user/1000/uml"
+	err := os.MkdirAll(nsDir, 0755)
+	if err != nil {
+		return err
+	}
+	err = syscall.Mount("", nsDir, "none", syscall.MS_SHARED|syscall.MS_REC, "")
+	if err == syscall.EINVAL {
+		err = syscall.Mount(nsDir, nsDir, "none", syscall.MS_BIND|syscall.MS_REC, "")
+		if err != nil {
+			return fmt.Errorf("mount --rbind %s %s failed: %q", nsDir, nsDir, err)
+		}
+		err = syscall.Mount("", nsDir, "none", syscall.MS_SHARED|syscall.MS_REC, "")
+	} else if err != nil {
+		return fmt.Errorf("mount --make-rshared %s failed: %q", nsDir, err)
+	}
+	// TODO dont hardcode
+	nsPath := filepath.Join(nsDir, "net-ns")
+	f, err := os.OpenFile(nsPath, os.O_CREATE|os.O_EXCL, 0444)
+	f.Close()
+	// Ensure the mount point is cleaned up on errors; if the namespace
+	// was successfully mounted this will have no effect because the file
+	// is in-use - from containernetworking/plugins/pkg/testutils
+	defer os.RemoveAll(nsPath)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		runtime.LockOSThread()
+		// Don't unlock. By not unlocking, golang will kill the OS thread when the
+		// goroutine is done (for go1.10+)
+		// https://github.com/containernetworking/plugins/blob/2c46a726805bcf13e2f78580c57b21e9de107285/pkg/testutils/netns_linux.go
+		defer wg.Done()
+		origNS, err := ns.GetNS(fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), unix.Gettid()))
+		if err != nil {
+			return
+		}
+		defer origNS.Close()
+
+		err = syscall.Unshare(syscall.CLONE_NEWNET)
+		if err != nil {
+			return
+		}
+
+		defer origNS.Set()
+		fmt.Println("mounting to", nsPath)
+		err = syscall.Mount(fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), unix.Gettid()), nsPath, "bind", syscall.MS_BIND, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	wg.Wait()
+	return err
+}
+
 // Wrapper arround containernetworking's netns helper
 // fills in the pid for the uml namespace
 // the function toRun will be run within the uml net namespace
 // opened by NewNS()
 func WithNetNS(toRun func(ns.NetNS) error) error {
-	pid, err := ioutil.ReadFile("/run/user/1000/uml/ns-pid")
-	if err != nil {
-		return err
-	}
-	nsPath := fmt.Sprintf("/proc/%s/ns/net", string(pid))
+	// TODO dont hardcode
+	nsPath := "/run/user/1000/uml/net-ns"
 	return ns.WithNetNSPath(nsPath, toRun)
 }
