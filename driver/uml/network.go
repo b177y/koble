dc582db2529e9c5b3e305792ac3355f27ce7e042
b177y <34008579+b177y@users.noreply.github.com>
2021-12-20 13:36:59 +0000

    Added tapout to vecnet

--- driver/uml/network.go
+++ driver/uml/network.go
@@ -18,7 +18,7 @@ func (ud *UMLDriver) CreateNetwork(n driver.Network) (err error) {
 		return err
 	}
 	if n.External {
-		// TODO add slirp to bridge
+		return vecnet.SetupExternal("tap0", "br_"+n.Name, n.Namespace)
 	}
 	return nil
 }
--- driver/uml/vecnet/switch.go
+++ driver/uml/vecnet/switch.go
@@ -2,6 +2,10 @@ package vecnet
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
@@ -62,12 +66,41 @@ func AddHost(namespace, tapname, bridge string) error {
 
 // TODO remove host
 
-func AddTapout(iface, network, namespace string) error {
-	// exec slirp4netns with params
-	// create bridge
-	// add host to bridge
-	// add slirp's iface to bridge
-	return nil
+func SetupExternal(iface, network, namespace string) error {
+	nsPath := filepath.Join("/run/user", os.Getenv("UML_ORIG_UID"), "uml/ns", namespace, "netns.bind")
+	cmd := exec.Cmd{
+		Path: "/usr/bin/slirp4netns",
+		Args: []string{"--configure", "--mtu=65520", "--disable-host-loopback",
+			"--disable-dns", "--netns-type=path", nsPath, iface},
+	}
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	return WithNetNS(namespace, func(ns.NetNS) error {
+		var tap netlink.Link
+		var err error
+		// timeout ~5s to wait for slirp4netns to create new tap
+		for i := 0; i < 10; i++ {
+			tap, err = netlink.LinkByName(iface)
+			if err == nil {
+				break
+			}
+			time.Sleep(500 * time.Millisecond)
+		}
+		if err != nil {
+			return fmt.Errorf("Error finding tap %s: %w", network, err)
+		}
+		br, err := netlink.LinkByName(network)
+		if err != nil {
+			return fmt.Errorf("Error finding bridge %s: %w", network, err)
+		}
+		err = netlink.LinkSetUp(tap)
+		if err != nil {
+			return err
+		}
+		return netlink.LinkSetMaster(tap, br)
+	})
 }
 
 // TODO remove tapout
