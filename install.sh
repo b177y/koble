f1f5707195b47d917f42e695e891ad24eb69ed92
b177y <34008579+b177y@users.noreply.github.com>
2022-02-02 23:53:28 +0000

    Fix install script for ubuntu 20.04

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ fi
 
 # Install podman dependencies
 sudo apt install ca-certificates
+wget https://packagecloud.io/shiftkey/desktop/gpgkey -O - | sudo apt-key add -
 source /etc/os-release
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
